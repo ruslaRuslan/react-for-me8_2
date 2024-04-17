7e45b50db01dacafadb7609a74051ef99b07bafa
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-17 14:09:02 +0400

    datalari verdik

--- src/pages/Github/index.jsx
+++ src/pages/Github/index.jsx
@@ -1,16 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./index.module.css";
 import axios from "axios";
 
 const API_GITHUB_URL = "https://api.github.com/users/Ali-GreenHeart";
 
 const GithubMain = () => {
-
   const [data, setData] = useState({});
 
   useEffect(() => {
     axios.get(API_GITHUB_URL).then(({ data }) => {
-      
+      setData(data);
     });
   }, []);
 
@@ -21,15 +20,15 @@ const GithubMain = () => {
         <button>search</button>
       </div>
       <div className={styles.dataContainer}>
-        <p>avatar_url</p>
-        <p>html_url</p>
-        <p>followers_url</p>
-        <p>following_url</p>
-        <p>repos_url</p>
-        <p>name</p>
-        <p>location</p>
-        <p>bio</p>
-        <p>followers(for count)</p>
+        <p><b>Name:</b>{data.name}</p>
+        <p><b>Bio:</b>{data.bio}</p>
+        <p><b>Location:</b>{data.location}</p>
+        <p><b>Follower count:</b>{data.followers} <a href={data.followers_url}>link</a></p>
+        <p><b></b><img style={{with:150, height: 150}} src={data.avatar_url} alt="" /></p>
+        <p></p>
+        <p><b>to account:</b>{data.html_url}</p>
+        <p><b>to repos:</b>{data.repos_url}</p>
+        <p>{data.bio}</p>
       </div>
     </>
   );
