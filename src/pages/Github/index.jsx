d1df23bbc745dbaecb0a28ebe05829d592db2bbd
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-17 13:17:10 +0400

    GithubMain-in icine p teqlerinde yazilari elave etdik

--- src/pages/Github/index.jsx
+++ src/pages/Github/index.jsx
@@ -1,16 +1,24 @@
-import styles from './index.module.css'
+import styles from "./index.module.css";
 const GithubMain = () => {
   return (
-   <>
-    <div className={styles.inputContainer}>
+    <>
+      <div className={styles.inputContainer}>
         <input type="text" />
         <button>search</button>
-    </div>
-    <div>
-
-    </div>
+      </div>
+      <div>
+        <p>avatar_url</p>
+        <p>html_url</p>
+        <p>followers_url</p>
+        <p>following_url</p>
+        <p>repos_url</p>
+        <p>name</p>
+        <p>location</p>
+        <p>bio</p>
+        <p>followers(for count)</p>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default GithubMain
+export default GithubMain;
